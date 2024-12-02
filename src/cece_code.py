7f8c3c72ae9d9531e091eeb0479a59331497f9b2
cecelia crounse <ceceliacrounse@gmail.com>
2024-12-02 17:36:43 -0500

    Add code for prettifying output, and more test strings

--- src/cece_code.py
+++ src/cece_code.py
@@ -1,15 +1,42 @@
+"""
+Final Project Cecelia and Ellie
+main code
+12/5/24
+CS1210A
+"""
 import time
 import wagner_fischer
+import random
 
-TEST = 'The quick brown fox jumps over the lazy dog.'
+TESTPHRASE = ['The quick brown fox jumps over the lazy dog.',
+              'Don’t put all your eggs in one basket.',
+              'One small step for man, one giant leap for mankind - Neil Armstrong 1969',
+              'Tongue Twister: She sells sea shells by the sea shore',
+              'I scream, you scream, we all scream for icecream',
+              '...and a partridge in a pear tree.']
+TESTCODING = ['if __name__ == "__main__":', 'public static void main (String[] args) {', 'print("Hello, World!")']
 
-start = time.time()
-user = input(f"Start Typing: {TEST} \n")
-end = time.time()
-difference = end - start
+def play(TEST):
+    start = time.time()
+    user = input(f"Start Typing: {TEST} \n")
+    end = time.time()
+    difference = end - start
+    errors = wagner_fischer.distance(user, TEST)
+    print(f"It took you {difference:.2f} seconds")
+    print(f"You made {errors} errors.")
+    print(f"Your accuracy was {100 - (errors/len(TEST)):.2%}")
 
-print(f"It took you {difference:.2f} seconds")
-
-errors = wagner_fischer.distance(user, TEST)
-
-print(f"You made {errors} errors.")
+if __name__ == "__main__":
+    while True:
+        uchoice = input("Welcome to Typing Test. Type 'p' to test with a phrase,"
+                    "'c' to test with code, and 'e' to End: ")
+        if uchoice == 'p':
+            play(random.choice(TESTPHRASE))
+        elif uchoice == 'c':
+            play(random.choice(TESTCODING))
+        elif uchoice == 'e':
+            print("Thank you for playing.")
+            break
+        else:
+            uchoice = input("That is not a correct input. Type 'p' to test with a phrase,"
+                            "'c' to test with code, and 'e' to End: ")
